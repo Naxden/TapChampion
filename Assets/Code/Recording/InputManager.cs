4774e7a753dea163917de4d5b6dc0bacb0be6222
Naxden <kilarjan05@gmail.com>
2022-12-29 11:24:58 +0100

    Input Update

--- Assets/Code/Recording/InputManager.cs
+++ Assets/Code/Recording/InputManager.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 using Saving.Note;
+using Recording.Note.Selectable;
 
 namespace Recording.InputManager
 {
     public class InputManager : MonoBehaviour
     {
         [SerializeField]
-        NoteRenderer.NoteRenderer noteRenderer;
+        private Recorder recorder;
+        [SerializeField]
+        private NoteRenderer.NoteRenderer noteRenderer;
         private KeyCode[] keys = {KeyCode.D, KeyCode.F, KeyCode.J, KeyCode.K, KeyCode.L};
         private float[] pushTimers = new float[5];
         private float[] addNotePositions = new float[5];
@@ -14,32 +17,86 @@ namespace Recording.InputManager
         public float noteDelay = 0.175f;
         private bool inputEnabled = false;
 
+        private GameObject selectedNote;
+
+        // function called externally by Event
         public void EnableInput()
         {
             inputEnabled = true;
         }
 
+        // function called externally by Event
         public void DiasableInput()
         {
             inputEnabled = false;
         }
 
-        // Update is called once per frame
         void Update()
         {
             if (inputEnabled)
             {
-                if (Input.GetMouseButtonDown(0))
-                {
-                    noteRenderer.AddNote(Camera.main.ScreenToWorldPoint(Input.mousePosition), NoteType.LongEnd);
-                }
+                CheckMouseInput();
 
                 CheckButtonsInput();
+
+                CheckSpaceInput();
             }
+        }
+
+        private void CheckMouseInput()
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (Input.GetMouseButtonDown(0))
+                MouseSingleClick(mousePosition);
+            if (Input.GetMouseButton(0))
+                MousePushDown(mousePosition);
+            if (Input.GetMouseButtonUp(0))
+                MouseRelease(mousePosition);
+        }
+
+        private void MouseSingleClick(Vector3 mousePosition)
+        {
+            recorder.PauseSong();
+
+            if (selectedNote != null)
+                selectedNote.GetComponent<Selectable>().Deselect();
+
+            if (mousePosition.y < -4.25f || mousePosition.y > 1.5f)
+                return;
+
+            selectedNote = noteRenderer.IsNoteThere(mousePosition);
+
+            if (selectedNote != null)
+            {
+                selectedNote.GetComponent<Selectable>().Select();
+            }
+            else
+            {
+                Debug.Log(mousePosition);
+                noteRenderer.AddNote(mousePosition, NoteType.LongEnd);
+            }
+        }
+
+        private void MousePushDown(Vector3 mousePosition)
+        {
+            if (mousePosition.y < -4.25f || mousePosition.y > 1.5f)
+                return;
+
+            if (selectedNote != null)
+                selectedNote.GetComponent<Selectable>().Move(mousePosition);
+        }
+
+        private void MouseRelease(Vector3 mousePosition)
+        {
+            if (mousePosition.y < -4.25f || mousePosition.y > 1.5f)
+                return;
         }
 
         private void CheckButtonsInput()
         {
+            float xPosition = transform.position.x;
+            
             for (int i = 0; i < 5; i++)
             {
                 if (!longNotesBeginAdded[i] && pushTimers[i] > noteDelay)
@@ -48,13 +105,13 @@ namespace Recording.InputManager
                     longNotesBeginAdded[i] = true;
                 }
                 if (Input.GetKeyDown(keys[i]))
-                    addNotePositions[i] = transform.position.x - 5f;
+                    addNotePositions[i] = xPosition - 5f;
                 if (Input.GetKey(keys[i]))
                     pushTimers[i] += Time.deltaTime;
                 if (Input.GetKeyUp(keys[i]))
                 {
                     if (longNotesBeginAdded[i])
-                        addNotePositions[i] = transform.position.x - 5f;
+                        addNotePositions[i] = xPosition - 5f;
 
                     PlaceNoteByKey(i, false);
                     pushTimers[i] = 0f;
@@ -74,12 +131,21 @@ namespace Recording.InputManager
 
             noteRenderer.AddNote(position, noteType);
         }
-        // external Slider calling the method
+
+        void CheckSpaceInput()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (recorder.songIsPlaying)
+                    recorder.PauseSong();
+                else
+                    recorder.PlaySong();
+            }
+        }
+
+        // function called externally by Silder's Event
         public void MoveCamera(float time)
         {
-            // accuracy to 0.1 of a second
-            // powiedzmy że alokuje 3000 GO dla każdego toru, i maks długość piosenki to 5 minut
-            // set propper multipler
             time *= 10;
             transform.position = new Vector3(time, 0f, -10f);
         }
